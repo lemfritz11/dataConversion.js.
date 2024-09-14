e156b548dcd48a9326974a4ca530065ecafe653f
lemfritz11 <aintdaiki@gmail.com>
2024-09-15 07:45:33 +0800

    initial commit with DataConversion.js

---
+++ DataConversion.js
@@ -0,0 +1,25 @@
+let price = "200";
+
+let priceAsInt = parseInt(price);
+console.log(priceAsInt);
+
+let priceAsNumber = Number(price);
+console.log(priceAsNumber); 
+
+let priceAsNumberPlus = +price;
+console.log(priceAsNumberPlus);
+
+
+let distance = 150;
+
+let distanceAsString = distance.toString();
+console.log(distanceAsString);  
+
+let distanceAsString2 = String(distance);
+console.log(distanceAsString2);
+
+
+let weight = "72.5";
+
+let weightAsDecimal = parseFloat(weight);
+console.log(weightAsDecimal);
